a0fe9ca6c47f6acdb8ff2a434d684df2eb2ced93
MoisturizedDragon <126834676+MoisturizedDragon@users.noreply.github.com>
2026-02-10 22:24:17 -0500

    Add rofi, slock, distrobox, and vscode

--- build_files/build.sh
+++ build_files/build.sh
@@ -5,14 +5,19 @@ set -ouex pipefail
 ### Install packages
 
 # Packages can be installed from any enabled yum repo on the image.
-dnf5 install -y tmux
+dnf5 install -y tmux rofi slock distrobox
 
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
+# VS Code from Microsoft repo
+tee /etc/yum.repos.d/vscode.repo <<'EOF'
+[code]
+name=Visual Studio Code
+baseurl=https://packages.microsoft.com/yumrepos/vscode
+enabled=1
+gpgcheck=1
+gpgkey=https://packages.microsoft.com/keys/microsoft.asc
+EOF
+sed -i "s/enabled=.*/enabled=0/g" /etc/yum.repos.d/vscode.repo
+dnf5 -y install --enablerepo=code code
 
 #### Enable System Services
 
